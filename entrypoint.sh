a10a87d226e80685d1ec24bf744720d2dd33be8b
valosnah <h.o.sund@gmail.com>
2022-03-16 15:24:13 +0100

    chore: singlequotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst "${ALTINN_HOST} ${ALTINN_API_KEY}" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${ALTINN_HOST} ${ALTINN_API_KEY}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "$DIGDIR_CRT_PEM" > /etc/nginx/client.crt.pem
 echo "$DIGDIR_KEY_PEM" > /etc/nginx/client.key.pem
